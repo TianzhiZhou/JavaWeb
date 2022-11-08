67979d48a01e31941ad89a85fec12ee46242e6e4
ztz <25876744+TianzhiZhou@users.noreply.github.com>
2022-11-08 21:02:00 +0800

    简易HttpServlet继承

---
+++ servlet/src/cn/ztz/HelloServlet2.java
@@ -0,0 +1,24 @@
+package cn.ztz;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: ztz
+ * @description:
+ * @date: 2022-11-08 20:57
+ */
+public class HelloServlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet2处理get请求");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet2处理post请求");
+    }
+}
