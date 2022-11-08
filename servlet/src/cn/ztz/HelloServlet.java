7dc24f7de508ee5ba183758d0d8b54aef0286d24
ztz <25876744+TianzhiZhou@users.noreply.github.com>
2022-11-08 20:51:21 +0800

    Servlet请求分发

--- servlet/src/cn/ztz/HelloServlet.java
+++ servlet/src/cn/ztz/HelloServlet.java
@@ -1,6 +1,7 @@
 package cn.ztz;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -9,9 +10,14 @@ import java.io.IOException;
  * @date: 2022-11-07 22:29
  */
 public class HelloServlet implements Servlet {
+
+    public HelloServlet() {
+        System.out.println("1 构造器方法");
+    }
+
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-
+        System.out.println("2 初始化方法");
     }
 
     @Override
@@ -21,16 +27,49 @@ public class HelloServlet implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Hello Servlet被访问");
+        System.out.println("3 service方法 == Hello Servlet被访问");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String method = request.getMethod();
+        System.out.println(method);
+        if("GET".equalsIgnoreCase(method)){
+            doGet();
+        }else if("POST".equalsIgnoreCase(method)){
+            doPost();
+        }
+
+    }
+
+    /**
+     * 处理get请求方法
+     * @author ztz
+     * @date 2022/11/8
+     * @param
+     * @return
+     */
+    public void doGet(){
+        System.out.println("处理get请求");
+    }
+
+    /**
+     * 处理post请求方法
+     * @author ztz
+     * @date 2022/11/8
+     * @param
+     * @return
+     */
+    public void doPost(){
+        System.out.println("处理post请求");
     }
 
     @Override
     public String getServletInfo() {
+        System.out.println("4 获取servlet信息方法");
         return null;
     }
 
     @Override
     public void destroy() {
-
+        System.out.println("5 销毁方法");
     }
 }
